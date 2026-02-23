5c80cd5136ba2e86ffd13973e9fbe6730c1b6c3d
kamleong <kamleong@users.noreply.github.com>
2026-02-23 09:40:06 +0800

    Update rss.py

--- rss.py
+++ rss.py
@@ -17,22 +17,25 @@ currtime = currtimeLocal.astimezone(tz)
 #print(str(currtimeLocal) + " [ " + currtime.strftime('%H:%M:%S %z') + " == " + currtimeUTC.strftime('%H:%M:%S %z') + " ] " + str(currtime-currtimeUTC))
 #quit()
 
+opt = '' if (len(sys.argv)<3) else sys.argv[2]
 url = 'https://news.google.com/rss?gl=MY' if (len(sys.argv)<2) else sys.argv[1]
 resp = requests.get(url)
 resp.raise_for_status()
 
 #tree = ET.parse('rss.xml'); root = tree.getroot()
 root = ET.fromstring(resp.content)
-rssItems = {}
+i = 0; rssPubDate = {}; rssTitle = {}; rssUrl = {}
 #for p in root.findall('.//programme[@channel="677fb08f5b68381e2dbfd5af"]'):
 for p in root.findall('.//item'):
+  i = i + 1
   pubDate = p.find('pubDate').text
   #print(pubDate) ; break;
   tzFormat = '%z' if re.search('\+\d{4}$', pubDate) else '%Z'
   pubDate = datetime.strptime(pubDate, '%a, %d %b %Y %H:%M:%S '+tzFormat).astimezone(tz)
   #print(pubDate) ; break;
-  title = p.find('title').text
-  rssItems[title] = pubDate
+  rssPubDate[i] = pubDate
+  rssTitle[i] = p.find('title').text
+  rssUrl[i] = p.find('link').text
   #print(
   #  pubDate.strftime('%Y-%m-%d %H:%M:%S') + " > " +
   #  title
@@ -41,22 +44,38 @@ for p in root.findall('.//item'):
   #break
 #
 
-rssItems = {k: v for k, v in sorted(rssItems.items(), key=lambda item: item[1], reverse=True)}
-print("last <pubDate> " + str(next(iter(rssItems.values()))))
+rssPubDate = {k: v for k, v in sorted(rssPubDate.items(), key=lambda item: item[1], reverse=True)}
+print("last [pubDate] " + str(next(iter(rssPubDate.values()))))
 
-termsz = os.get_terminal_size(0)
-## specify fd=sys.stdin.fileno() explicitly above for compatibility with piping and watch command, to avoid OSError: [Errno 25] Inappropriate ioctl for device
-i = 0 ; maxline = termsz.lines-2 ; maxlen = termsz.columns-1
+maxline = 100 ; maxlen = 300
 
-for key, val in rssItems.items():
+if opt=="HTML" :
+  print("*HTML*<br><br>")
+else :
+  termsz = os.get_terminal_size(0)
+  ## specify fd=sys.stdin.fileno() explicitly above for compatibility with piping and watch command, to avoid OSError: [Errno 25] Inappropriate ioctl for device
+  maxline = termsz.lines-2 ; maxlen = termsz.columns-1
+#
+
+i = 0
+for key, val in rssPubDate.items():
 #for key in rss_items.keys():
-  i = i + 1 ; title = key ; pubDate = val
-  print(
-    #pubDate.strftime('%H:%M:%S') +
-    ">" + key[:maxlen]
-  )
+  i = i + 1 ; pubDate = val
+  if opt=="HTML" :
+    print(
+      #pubDate.strftime('%H:%M:%S') +
+      "<li style='list-style-type:none;padding-left:15px;text-indent:-15px;' xonclick=\"window.open('" + rssUrl[key] + "')\">"
+      + "<a target='_blank' href='" + rssUrl[key] + "'><small><small>&gt;</small></small></a> "
+      + rssTitle[key]
+    )
+  else :
+    print(
+      #pubDate.strftime('%H:%M:%S') +
+      ">" + rssTitle[key][:maxlen]
+    )
+    if i >= maxline : break
+  #
   #print(currtime) ; #print(pubDate)
   #break
-  if i >= maxline : break
   if (currtime-pubDate) > timedelta(hours=18) : break
 #
